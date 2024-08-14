0d1e0b7dc19e2a425f29a393c9b70475eb244699
rohitkumar502 <rohitsaura398@gmail.com>
2024-08-14 06:14:38 +0530

    RatingService with ResponseEntity under UserService external

--- UserService/src/main/java/com/ms/user/service/external/services/RatingService.java
+++ UserService/src/main/java/com/ms/user/service/external/services/RatingService.java
@@ -2,6 +2,7 @@ package com.ms.user.service.external.services;
 
 import com.ms.user.service.entity.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +14,19 @@ public interface RatingService {
 
     //GET
     @GetMapping("/ratings")
-    List<Rating> getRatings();
+    public ResponseEntity<List<Rating>> getRatings();
 
     //POST
     @PostMapping("/ratings")
-    Rating createRating(Rating rating);
+    public ResponseEntity<Rating> createRating(Rating rating);
 
     //PUT
     @PutMapping("/ratings/{ratingId}")
-    Rating updateRating(@PathVariable String ratingId, Rating rating);
+    public ResponseEntity<Rating> updateRating(@PathVariable String ratingId, Rating rating);
 
     //DELETE
     @DeleteMapping("/ratings/{ratingId}")
-    void deleteRating(@PathVariable String ratingId);
+    public ResponseEntity<Void> deleteRating(@PathVariable String ratingId);
     
 
 }
--- UserService/src/test/java/com/ms/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/ms/user/service/UserServiceApplicationTests.java
@@ -6,6 +6,7 @@ import com.ms.user.service.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -18,14 +19,16 @@ class UserServiceApplicationTests {
     @Autowired
     private RatingService ratingService;
 
-//    @Test
-//    void createRating() {
-//        Rating rating = Rating.builder().rating(10).userId("")
-//                .hotelId("").feedback("This is created using feign client").build();
-//
-//        Rating savedRating = ratingService.createRating(rating);
-//        System.out.println("new rating created: " + savedRating);
-//    }
+    @Test
+    void createRating() {
+        Rating rating = Rating.builder().rating(10).userId("")
+                .hotelId("").feedback("This is created using feign client").build();
+
+        ResponseEntity<Rating> ratingResponseEntity = ratingService.createRating(rating);
+        Rating savedRating = ratingResponseEntity.getBody();
+        System.out.println("new rating created: " + savedRating);
+
+    }
 
 
 
